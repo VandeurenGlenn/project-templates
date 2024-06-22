614a554ac56a34d1eff4fbfd369a62f2e27d3c89
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-22 11:56:40 +0200

    rewrite to module & cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
-const readFileSync = require('fs').readFileSync;
-const npmPackage = readFileSync('package.json', 'utf8');
-const { version, name } = JSON.parse(npmPackage);
-const production = Boolean(process.argv[2] === 'production');
+import { readFile } from 'fs/promises'
+
+const npmPackage = await readFile('package.json', 'utf8')
+const { version, name } = JSON.parse(npmPackage)
+// const production = Boolean(process.argv[2] === 'production');
+
 export default [
-	{
-		input: ['src/index.js', 'src/project-serve.js'],
-		output: {
-			dir: './',
-			format: 'cjs',
-			sourcemap: false,
-			intro: `const ENVIRONMENT = {version: '${version}', production: true};`,
-			banner: `/* ${name} version ${version} */`
-		},
-		experimentalCodeSplitting: true,
-		experimentalDynamicImport: true
-	}
-];
+  {
+    input: ['src/serve.js', 'src/install.js', 'src/stream.js', 'src/upgrade.js', 'src/build.js'],
+    output: {
+      dir: './dist',
+      format: 'es',
+      sourcemap: false,
+      intro: `const ENVIRONMENT = {version: '${version}', production: true};`,
+      banner: `/* ${name} version ${version} */`
+    }
+  },
+  {
+    input: ['src/index.js'],
+    output: {
+      external: ['build', 'serve', 'install', 'stream', 'upgrade'],
+      dir: './dist',
+      format: 'es',
+      sourcemap: false,
+      banner: '#!/usr/bin/env node'
+    }
+  }
+]
--- src/index.js
+++ src/index.js
@@ -1,52 +1,55 @@
-import { basename, join } from 'path';
-import { promisify } from 'util';
-import { homedir } from 'os';
-import fs from 'fs';
-import { prompt } from 'inquirer';
+import { basename, join } from 'path'
+import { homedir } from 'os'
+import { readFile, writeFile } from 'fs'
+import inquirer from 'inquirer'
 import meow from 'meow'
-import projectStream from './project-stream';
-import projectInstall from './project-install';
-import projectUpgrade from './project-upgrade';
-import projectBuild from './project-build';
+import projectStream from './stream.js'
+import projectInstall from './install.js'
+import projectUpgrade from './upgrade.js'
+import projectBuild from './build.js'
 
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
-
-const configPath = join(homedir(), '.project-templates');
+const configPath = join(homedir(), '.project-templates')
 
 const getConfig = async () => {
   try {
     let config = await readFile(configPath)
-    config = JSON.parse(config.toString());
-    return config;
+    config = JSON.parse(config.toString())
+    return config
   } catch (e) {
+    console.log(e)
     if (e.code === 'ENOENT') {
-      const github = {};
-      const { name } = await prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'github username'
-      }])
-      const { email } = await prompt([{
-        type: 'input',
-        name: 'email',
-        message: 'github user email',
-        default: `${name}@gmail.com`
-      }])
+      const github = {}
+      const { name } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'github username'
+        }
+      ])
+      const { email } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'email',
+          message: 'github user email',
+          default: `${name}@gmail.com`
+        }
+      ])
 
-      const { license, defaultTemplate } = await prompt([{
-        type: 'input',
-        name: 'license',
-        message: 'preferred license'
-      }])
-      await writeFile(configPath, JSON.stringify({email, name, license, defaultTemplate}));
-      return await getConfig();
+      const { license, defaultTemplate } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'license',
+          message: 'preferred license'
+        }
+      ])
+      await writeFile(configPath, JSON.stringify({ email, name, license, defaultTemplate }))
+      return await getConfig()
     }
   }
 }
-(async () => {
-  const cli = meow(
-      `
+
+const cli = meow(
+  `
       Usage
         $ jsproject [option] <input>
 
@@ -94,79 +97,77 @@ const getConfig = async () => {
       - latest
 
       example: project upgrade latest
-      `, {
-        flags: {
-          template: {
-            type: 'string',
-            alias: 't'
-          },
-          name: {
-            type: 'string',
-            alias: 'n'
-          },
-          upgrade: {
-            type: 'string',
-            alias: 'u'
-          },
-          serve: {
-            type: 'string',
-            alias: 's'
-          },
-          run: {
-            type: 'string',
-            alias: 'r'
-          },
-          yesToAll: {
-            type: 'boolean',
-            alias: 'y'
-          },
-          port: {
-            type: 'number',
-            alias: 'p'
-          },
-          host: {
-            type: 'string',
-            alias: 'h'
-          },
-          skipInstall: {
-            type: 'boolean',
-            alias: 'S'
-          }
-        }
-      })
-  // console.log(cli);
-  const flags = cli.flags
-  console.log(flags);
-  const flagsLength = Object.keys(cli.flags).length
-  if (cli.input.length === 0 && flagsLength === 0) return cli.showHelp()
-  else if (cli.input.length !== 0 && flagsLength === 0 ||
-          cli.input.length !== 0 && flags.yesToAll) {
-    cli.flags.template = cli.input[0]
+      `,
+  {
+    importMeta: import.meta
+  },
+  {
+    flags: {
+      template: {
+        type: 'string',
+        alias: 't'
+      },
+      name: {
+        type: 'string',
+        alias: 'n'
+      },
+      upgrade: {
+        type: 'string',
+        alias: 'u'
+      },
+      serve: {
+        type: 'string',
+        alias: 's'
+      },
+      run: {
+        type: 'string',
+        alias: 'r'
+      },
+      yesToAll: {
+        type: 'boolean',
+        alias: 'y'
+      },
+      port: {
+        type: 'number',
+        alias: 'p'
+      },
+      host: {
+        type: 'string',
+        alias: 'h'
+      },
+      skipInstall: {
+        type: 'boolean',
+        alias: 'S'
+      }
+    }
   }
-  if (flags.serve) {
-    const importee = await import('./project-serve.js')
-    return importee.default(flags.serve, flags.port, flags.host)
-  }
-  const config = await getConfig();
-  let template = process.argv[2];
-  let project = process.argv[3];
+)
+// console.log(cli);
+const flags = cli.flags
+console.log({ flags })
+const flagsLength = Object.keys(cli.flags).length
+if (cli.input.length === 0 && flagsLength === 0) cli.showHelp()
+else if ((cli.input.length !== 0 && flagsLength === 0) || (cli.input.length !== 0 && flags.yesToAll)) {
+  cli.flags.template = cli.input[0]
+}
+if (flags.serve) {
+  const importee = await import('./serve.js')
+  importee.default(flags.serve, flags.port, flags.host)
+} else {
+  const config = await getConfig()
+  let template = process.argv[2]
+  let project = process.argv[3]
 
-
-  const dest = process.argv[4] || process.cwd();
+  const dest = process.argv[4] || process.cwd()
 
   if (!cli.flags.name) cli.flags.name = basename(process.cwd())
 
   if (cli.flags.template) {
-    config.project = cli.flags.name;
+    config.project = cli.flags.name
     config.author = `${config.name} <${config.email}>`
-    console.log(flags);
-    await projectStream(config, cli.flags.template, dest, flags.yesToAll);
+    console.log(flags)
+    await projectStream(config, cli.flags.template, dest, flags.yesToAll)
     if (!flags.skipInstall) await projectInstall(dest, cli.flags.upgrade)
     await projectBuild(dest)
-    return
   } else if (cli.flags.upgrade) await projectUpgrade(dest, cli.flags.upgrade)
-
-
-
-
-})()
+}
